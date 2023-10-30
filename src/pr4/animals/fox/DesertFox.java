46f090e02d47435a1adf8295db44a90c3c759a6f
rrrguez <alu0101394763@ull.edu.es>
2023-10-30 23:34:40 +0000

    Resubidas las fotos

--- src/pr4/animals/fox/DesertFox.java
+++ src/pr4/animals/fox/DesertFox.java
@@ -6,7 +6,7 @@ public class DesertFox extends Fox {
     @Override
     public File showPicture() {
         // Obtenemos la ruta de la imagen
-        String imagePath = "/src/images/desert_fox_2.jpg";
+        String imagePath = "/src/images/desert_fox.jpg";
         return new File(imagePath);
     }
 }
