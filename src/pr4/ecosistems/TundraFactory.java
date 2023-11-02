5e16b7a6f329db2b7472ba825f6a1eabe60d9c2e
Joan Marc Perea Llodra <129542109+J0anM4rc@users.noreply.github.com>
2023-11-02 18:54:30 +0000

    Add files via upload

--- src/pr4/ecosistems/TundraFactory.java
+++ src/pr4/ecosistems/TundraFactory.java
@@ -1,11 +1,12 @@
 package ecosistems;
 
 import animals.Animal;
+import animals.bear.TundraBear;
 import animals.fox.TundraFox;
 
 public class TundraFactory extends EcosystemFactory {
     @Override
     public Animal[] createEcosystem() {
-        return new Animal[]{new TundraFox()};
+        return new Animal[]{new TundraFox(),new TundraBear()};
     }
 }
