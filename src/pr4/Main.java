e3ad1b882332476d97ad7d47ab5452c3f4d430cc
Joan Marc Perea Llodra <129542109+J0anM4rc@users.noreply.github.com>
2023-11-02 18:53:47 +0000

    Add files via upload

--- src/pr4/Main.java
+++ src/pr4/Main.java
@@ -1,85 +1,162 @@
-import java.awt.Component;
-import java.awt.LayoutManager;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.imageio.plugins.jpeg.JPEGImageReadParam;
+import java.util.ArrayList;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 import ecosistems.*;
 import animals.*;
 
+//
+public class Main extends JFrame implements ActionListener{
 
-public class Main {
-    // Declaración de la ventana
-    static JFrame frame = new JFrame("DAP - Práctica 4: Factorías");
-
-    // Declaración de los paneles
-    // Los botones y las fotos irán en paneles separados dentro de la ventana
-    static JPanel ecoPanel = new JPanel();
-    static JPanel imagePanel = new JPanel();
+    // Declaración VARIABLES
 
     // Declaración de los botones de ecosistema
     // En función del botón que seleccione el usuario, se mostrarán unos animales u otros
-    static JButton desertButton = new JButton("Desierto");
-    static JButton forestButton = new JButton("Bosque");
-    static JButton jungleButton = new JButton("Selva");
-    static JButton mountainButton = new JButton("Montaña");
-    static JButton savannaButton = new JButton("Savana");
-    static  JButton tundraButton = new JButton("Tundra");
+    private JButton desertButton = new JButton("Desierto");
+    private JButton forestButton = new JButton("Bosque");
+    private JButton jungleButton = new JButton("Selva");
+    private JButton mountainButton = new JButton("Montaña");
+    private JButton savannaButton = new JButton("Savana");
+    private JButton tundraButton = new JButton("Tundra");
+
+    //Declaracion de los botones Animales
+    private ArrayList<JButton> btnAnimals = new ArrayList<>();
 
     // Declaración de fábrica de ecosistemas y del animal a mostrar
-    static EcosystemFactory factory = null;
-    static Animal animal = null;
+    private EcosystemFactory factory = null;
+    private Animal[] animal = null;
+
+    private JLabel labelImagen;
+
 
     public Main() {
-        // Configuración de la ventana
-        frame.setSize(900, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Configurar la ventana principal
+        super("Ventana Principal");
+        setSize(300, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
+        Image iconImage = Toolkit.getDefaultToolkit().getImage("DAP-Pr4-master/src/images/icono.jpeg");
+        setIconImage(iconImage);
+
 
         // Configuración del panel de los botones de ecosistema
-        ecoPanel.add(desertButton);
-        ecoPanel.add(forestButton);
-        ecoPanel.add(jungleButton);
-        ecoPanel.add(mountainButton);
-        ecoPanel.add(savannaButton);
-        ecoPanel.add(tundraButton);
+        desertButton.addActionListener(this);
+        forestButton.addActionListener(this);
+        jungleButton.addActionListener(this);
+        mountainButton.addActionListener(this);
+        savannaButton.addActionListener(this);
+        tundraButton.addActionListener(this);
 
-        frame.add(ecoPanel);
 
-        // Action listeners
-        // Aquí se define lo que ocurrirá cuando se pulse cada botón
-        desertButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                factory = new DesertFactory();
+        // Configurar el panel de botones en dos columnas
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BorderLayout());
 
-                Animal[] desert_animals = factory.createEcosystem();
+        // Agregar margen (espacio) encima de los botones
+        int topMargin = 20; // Puedes ajustar este valor según tus necesidades
+        buttonPanel.setBorder(new EmptyBorder(topMargin, 0, 0, 0));
 
-                BufferedImage bufferedImage = null;
+        // Configurar un panel para los botones en el centro
+        JPanel buttonCenterPanel = new JPanel();
+        buttonCenterPanel.setLayout(new GridLayout(0, 2, 20,20)); // 0 filas y 2 columnas
 
-                for (int i = 0; i < desert_animals.length; ++i) {
-                  try {
-                    bufferedImage = ImageIO.read(desert_animals[i].showPicture());
-                  } catch (IOException e1) {
-                    e1.printStackTrace();
-                  }
-                  ImageIcon imageIcon = new ImageIcon(bufferedImage);
+        // Agregar los botones al panel central
+        buttonCenterPanel.add(desertButton);
+        buttonCenterPanel.add(forestButton);
+        buttonCenterPanel.add(jungleButton);
+        buttonCenterPanel.add(mountainButton);
+        buttonCenterPanel.add(savannaButton);
+        buttonCenterPanel.add(tundraButton);
 
-                  JLabel jLabel = new JLabel();
+        // Agregar el panel central de botones al panel principal
+        buttonPanel.add(buttonCenterPanel, BorderLayout.CENTER);
 
-                  jLabel.setIcon(imageIcon);
-                  imagePanel.add(jLabel);
-                }
-            }
-          });
-        frame.setVisible(true);
+        // Agregar el panel de botones a la ventana principal
+        add(buttonPanel);
+
+
+
     };
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton) e.getSource();
+
+
+        //Comprueba si se ha pulsado en algun btnAnimal
+        for(int i = 0; i<btnAnimals.size();i++){
+            System.out.println("Btn Esperado: " + btnAnimals.get(i));
+
+            if(source == btnAnimals.get(i)){
+                ImageIcon imagenIcon = new ImageIcon(animal[i].showPicture()); // Reemplaza con la ruta de tu imagen
+                Image imagenOriginal = imagenIcon.getImage();
+                Image imagenEscalada = imagenOriginal.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+                ImageIcon imagenEscaladaIcon = new ImageIcon(imagenEscalada);
+
+                labelImagen.setIcon(imagenEscaladaIcon);
+                return;
+            }
+        }
+        //Limpia el Array de Botones cuando se cambia de Ecosistema
+        btnAnimals.clear();
+
+        //Comprueba si se ha elegido algun Ecosistema
+        if (source == desertButton) {
+            factory = new DesertFactory();
+            mostrarVentanaConBotones(factory.createEcosystem());
+        }else if(source == forestButton){
+            factory = new ForestFactory();
+            mostrarVentanaConBotones(factory.createEcosystem());
+        } else if(source == jungleButton){
+            factory = new JungleFactory();
+            mostrarVentanaConBotones(factory.createEcosystem());
+        }else if(source == mountainButton){
+            factory = new MountainFactory();
+            mostrarVentanaConBotones(factory.createEcosystem());
+        }else if(source == savannaButton){
+            factory = new SavannaFactory();
+            mostrarVentanaConBotones(factory.createEcosystem());
+        }else if(source == tundraButton){
+            factory = new TundraFactory();
+            mostrarVentanaConBotones(factory.createEcosystem());
+        }
+    }
+
+    public void mostrarVentanaConBotones(Animal[] animales) {
+        JFrame ventanaNueva = new JFrame("Elija un animal");
+        Image iconImage = Toolkit.getDefaultToolkit().getImage("DAP-Pr4-master/src/images/icono.jpeg");
+        ventanaNueva.setIconImage(iconImage);
+        ventanaNueva.setSize(500, 300);
+        // Crear botones en la ventana nueva
+        for(int i = 0; i< animales.length; i++){
+            animal = animales;
+            btnAnimals.add(new JButton(animales[i].getName()));
+            btnAnimals.get(i).addActionListener(this);
+            ventanaNueva.add(btnAnimals.get(i));
+        }
+        ventanaNueva.setLayout(new FlowLayout());
+        // Crear una etiqueta para mostrar la imagen
+        labelImagen = new JLabel();
+        ventanaNueva.add(labelImagen);
+        ventanaNueva.setLocationRelativeTo(null);
+
+
+        ventanaNueva.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra solo la ventana secundaria
+
+        ventanaNueva.setVisible(true);
+    }
+
+
+
     public static void main(String[] args) {
-      SwingUtilities.invokeLater(() -> new Main());
+        SwingUtilities.invokeLater(() -> {
+            Main ventanaPrincipal = new Main();
+            ventanaPrincipal.setVisible(true);
+        });
+
     }
 }
